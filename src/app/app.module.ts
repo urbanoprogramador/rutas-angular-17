83eb108ed0c187e33b3512bdf5953bfcff8903c1
urbanoprogramador <urbanoprogramador@gmail.com>
2019-10-08 13:10:39 -0400

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,27 @@
 import { NgModule }       from '@angular/core';
-import { BrowserModule }  from '@angular/platform-browser';
+import { CommonModule }   from '@angular/common';
 import { FormsModule }    from '@angular/forms';
 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppComponent }     from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import { HeroesModule }     from './heroes/heroes.module';
+import { AppComponent }            from './app.component';
+import { PageNotFoundComponent }   from './page-not-found/page-not-found.component';
+import { ComposeMessageComponent } from './compose-message/compose-message.component';
 
-import { CrisisListComponent }   from './crisis-list/crisis-list.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { MessageService } from './message.service';
+import { AppRoutingModule }        from './app-routing.module';
+import { HeroesModule }            from './heroes/heroes.module';
+import { CrisisCenterModule }      from './crisis-center/crisis-center.module';
 
 @NgModule({
   imports: [
-    BrowserModule,
+    CommonModule,
     FormsModule,
-    BrowserAnimationsModule,
     HeroesModule,
+    CrisisCenterModule,
     AppRoutingModule
   ],
   declarations: [
     AppComponent,
-    CrisisListComponent,
     PageNotFoundComponent
   ],
-  bootstrap: [ AppComponent ],
-  providers: [MessageService]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
