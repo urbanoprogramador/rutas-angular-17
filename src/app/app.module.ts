ba2a09ffff28b95f2d99e32e497250bc206ed912
urbanoprogramador <urbanoprogramador@gmail.com>
2019-10-07 22:23:48 -0400

    2 files modified, 12 added, 3 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,25 @@ import { FormsModule }    from '@angular/forms';
 
 import { AppComponent }     from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HeroesModule }     from './heroes/heroes.module';
 
 import { CrisisListComponent }   from './crisis-list/crisis-list.component';
-import { HeroListComponent }     from './hero-list/hero-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { MessageService } from './message.service';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
+    HeroesModule,
     AppRoutingModule
   ],
   declarations: [
     AppComponent,
-    HeroListComponent,
     CrisisListComponent,
     PageNotFoundComponent
   ],
-  bootstrap: [ AppComponent ]
+  bootstrap: [ AppComponent ],
+  providers: [MessageService]
 })
 export class AppModule { }
