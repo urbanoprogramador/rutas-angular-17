89c011f2cb023104080beee2c6b1180f01572c75
urbanoprogramador <urbanoprogramador@gmail.com>
2019-10-07 21:19:18 -0400

    3 files modified, 9 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { HeroListComponent } from './hero-list/hero-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  { path: 'crisis-center', component: CrisisListComponent },
+  { path: 'heroes', component: HeroListComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true ,useHash:true} // <-- debugging purposes only
+    )
+  ],
+  declarations: [ AppComponent, CrisisListComponent, HeroListComponent, PageNotFoundComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
